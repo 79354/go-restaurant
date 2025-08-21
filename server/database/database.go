79e16d44d08e9874aeb7a248290112eda5d55456
79354 <gillramansingh900@gmail.com>
2025-08-22 00:28:22 +0530

    changed initialization for collections

--- server/database/database.go
+++ server/database/database.go
@@ -1,3 +1,4 @@
+// database/database.go
 package database
 
 import (
@@ -14,35 +15,57 @@ import (
 
 var Client *mongo.Client
 
-func DBinstance(){
+func DBinstance() *mongo.Client {
 	err := godotenv.Load(".env")
-	if err != nil{
-		log.Fatal("Error loading the .env file")
+	if err != nil {
+		log.Println("No .env file found, using system env")
 	}
 
-	MongoDB := os.Getenv("MONGODB_URL")
-	if MongoDB == ""{
-		MongoDB = "mongodb://localhost:27017"
+	mongoURL := os.Getenv("MONGO_URI")
+	if mongoURL == "" {
+		mongoURL = "mongodb://localhost:27017"
 	}
-	
+
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI(MongoDB))
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(mongoURL))
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("Failed to connect to MongoDB:", err)
 	}
 
 	err = client.Ping(ctx, nil)
-	if err != nil{
-		log.Fatal(err)
+	if err != nil {
+		log.Fatal("Failed to ping MongoDB:", err)
 	}
 
-	fmt.Println("Connected to mongodb!")
-
+	fmt.Println("Connected to MongoDB!")
 	Client = client
+	return client
 }
 
-func OpenCollection(client *mongo.Client, name string) *mongo.Collection{
+func OpenCollection(client *mongo.Client, name string) *mongo.Collection {
 	return client.Database("restaurant").Collection(name)
+}
+
+var (
+	UserCollection        *mongo.Collection
+	FoodCollection        *mongo.Collection
+	MenuCollection        *mongo.Collection
+	TableCollection       *mongo.Collection
+	OrderCollection       *mongo.Collection
+	OrderItemCollection   *mongo.Collection
+	InvoiceCollection     *mongo.Collection
+)
+
+func InitializeCollections() {
+	UserCollection = OpenCollection(Client, "user")
+	FoodCollection = OpenCollection(Client, "food")
+	MenuCollection = OpenCollection(Client, "menu")
+	TableCollection = OpenCollection(Client, "table")
+	OrderCollection = OpenCollection(Client, "order")
+	OrderItemCollection = OpenCollection(Client, "orderItem")
+	InvoiceCollection = OpenCollection(Client, "invoice")
+
+	log.Println("Collections initialized")
 }
